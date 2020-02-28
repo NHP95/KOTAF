5c4dd778d0b26f193caba789b37c2e5b04e30236
Phu Huu Nguyen <phuhnguyen@kms-technology.com>
2020-02-28 16:26:42 +0700

    Can pass in configuration file name using property 'config'

--- src/main/kotlin/com/coe/kotaf/Configuration.kt
+++ src/main/kotlin/com/coe/kotaf/Configuration.kt
@@ -4,7 +4,8 @@ import com.beust.klaxon.JsonObject
 import com.beust.klaxon.Parser
 import com.coe.kotaf.utils.getResourcesPath
 
+val configurationFilePath: String = System.getProperty("config", "configuration.json")
 val configuration: JsonObject =
-    Parser.default().parse(getResourcesPath("configuration.json")) as JsonObject
+    Parser.default().parse(getResourcesPath(configurationFilePath)) as JsonObject
 
 val ELEMENT_TIMEOUT = configuration.long("elementTimeout") ?: 10
--- src/main/kotlin/com/coe/kotaf/utils/FileUtils.kt
+++ src/main/kotlin/com/coe/kotaf/utils/FileUtils.kt
@@ -3,9 +3,6 @@ package com.coe.kotaf.utils
 import java.io.File
 import java.net.URLDecoder
 
-/*
-TODO : display a friendly message
- */
 fun getResourcesPath(relativePath: String, enc: String = "UTF-8"): String {
     if (relativePath.startsWith(File.separator)) {
         throw IllegalArgumentException("Path must not start with ${File.separator}")
